d02d7de2a09da07b21c72e4e7b6112fc0525f61f
gbouras13 <george.bouras@adelaide.edu.au>
2026-03-11 10:13:18 +1030

    patch model_config for lower memory

--- src/alphafold3/model/model_config.py
+++ src/alphafold3/model/model_config.py
@@ -24,9 +24,16 @@ class GlobalConfig(base_config.BaseConfig):
   bfloat16: Literal['all', 'none', 'intermediate'] = 'all'
   final_init: Literal['zeros', 'linear'] = 'zeros'
   pair_attention_chunk_size: Sequence[_Shape2DType] = ((1536, 128), (None, 32))
+#   pair_transition_shard_spec: Sequence[_Shape2DType] = (
+#       (2048, None),
+#       (None, 1024),
+#   )
+# see https://github.com/google-deepmind/alphafold3/blob/main/docs/performance.md#unified-memory
   pair_transition_shard_spec: Sequence[_Shape2DType] = (
       (2048, None),
-      (None, 1024),
+      (3072, 1024),
+      (None, 512),
   )
+  
   # Note: flash_attention_implementation = 'xla' means no flash attention.
   flash_attention_implementation: attention.Implementation = 'triton'
